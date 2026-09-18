31281726668c69531cbfdc3e87735ea3dfcb4e25
Lê ANh Quân <22t1020710@husc.edu.vn>
2026-09-19 01:51:34 +0700

    Bai 1 Bien va Kieu du lieu

---
+++ bai_1_variables_and_data_types.py
@@ -0,0 +1,39 @@
+name = 'John Doe'
+age = 12
+print(name)
+print(age)
+
+#data types
+#integer
+my_integer_var = 10    
+print('Integer:', my_integer_var)
+
+#float
+my_float_var = 10.5
+print('Float:', my_float_var)
+
+#string
+my_string_var = 'hello'
+print('String:', my_string_var) 
+
+#boolean
+my_boolean_var = True
+print('Boolean: ', my_boolean_var)
+
+#distinguishing between data types
+developer = 'Devin'
+print(type(developer))
+
+#function types() print for each one
+my_integer_var = 10
+print(type(my_integer_var))
+my_float_var = 5
+print(type(my_float_var))
+my_string_var = 'Fuk'
+print(type(my_string_var))
+my_boolean_var = False
+print(type(my_boolean_var))
+
+#check isinstance() function with data types
+account_balance = '12'
+print(isinstance(account_balance,(int,float)))
