e1bcbec0f7ea43e059a4fa10dd904104706d3173
Lê ANh Quân <22t1020710@husc.edu.vn>
2026-09-19 02:34:41 +0700

    sua ten

---
+++ bai_2_build_a_report_card_printer.py
@@ -0,0 +1,12 @@
+name = 'Alice'
+print(name)
+print(type(name))
+is_student = True #Declare is_student is True
+print(is_student,type(is_student))#Print both same line
+print(name,type(name))
+age = 20
+print(age,type(age))
+
+score = 80.5
+print(isinstance(score, int))#Check score is int or false
+print(isinstance(score, float))
